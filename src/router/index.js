8d6f82bc61304556362fea32e5b980d496c79c7c
Ehsanul Haque <ehsan.nalta@gmail.com>
2020-05-11 14:53:08 -0400

    creating routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Brazil from "../views/Brazil.vue";
+import Panama from "../views/Panama.vue";
+import Hawaii from "../views/Hawaii.vue";
+import Jamaica from "../views/Jamaica.vue";
 
 Vue.use(VueRouter);
 
@@ -18,7 +22,27 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  {
+    path: "/brazil",
+    name: "Brazil",
+    component: Brazil
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: Panama
+  },
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: Hawaii
+  },
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: Jamaica
+  },
 ];
 
 const router = new VueRouter({
