b6d88ad1d297945d9a694a795c71b3e0cb0bacb3
Ehsanul Haque <ehsan.nalta@gmail.com>
2020-05-11 15:21:44 -0400

    lazy load

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "../views/Brazil.vue";
-import Panama from "../views/Panama.vue";
-import Hawaii from "../views/Hawaii.vue";
-import Jamaica from "../views/Jamaica.vue";
 
 Vue.use(VueRouter);
 
@@ -26,22 +22,26 @@ const routes = [
   {
     path: "/brazil",
     name: "Brazil",
-    component: Brazil
+    component: () =>
+        import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue")
   },
   {
     path: "/panama",
     name: "Panama",
-    component: Panama
+    component: () =>
+        import(/* webpackChunkName: "panama" */ "../views/Panama.vue")
   },
   {
     path: "/hawaii",
     name: "Hawaii",
-    component: Hawaii
+    component: () =>
+        import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue")
   },
   {
     path: "/jamaica",
     name: "Jamaica",
-    component: Jamaica
+    component: () =>
+        import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue")
   },
 ];
 
